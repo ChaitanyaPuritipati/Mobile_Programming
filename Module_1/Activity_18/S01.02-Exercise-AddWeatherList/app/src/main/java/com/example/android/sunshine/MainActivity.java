5d6ca9c8b35655e163c1623c27e9563c71dd018f
ChaitanyaPuritipati <chaitanya.manipal@msitprogram.net>
2019-03-12 00:39:52 +0530

    Added S2 in A18 in M1 MP

---
+++ Module_1/Activity_18/S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.sunshine;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    TextView weatherview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forecast);
+
+
+        weatherview = (TextView) findViewById(R.id.tv_weather_data);
+
+        String[] weatherinfo = {
+                "Today, May 17 - Clear - 17 oC",
+                "Today, May 17 - Clear - 17 oC",
+                "Today, May 17 - Clear - 17 oC",
+                "Today, May 17 - Clear - 17 oC",
+                "Today, May 17 - Clear - 17 oC",
+                "Today, May 17 - Clear - 18 oC",
+                "Today, May 17 - Clear - 17 oC",
+                "Today, May 17 - Clear - 19 oC",
+                "Today, May 17 - Clear - 27 oC",
+                "Today, May 17 - Clear - 27 oC",
+                "Today, May 17 - Clear - 27 oC",
+                "Today, May 17 - Clear - 27 oC",
+                "Today, May 17 - Clear - 27 oC",
+                "Today, May 17 - Clear - 27 oC",
+                "Today, May 17 - Clear - 27 oC",
+                "Today, May 17 - Clear - 27 oC",
+                "Today, May 17 - Clear - 27 oC",
+                "Today, May 17 - Clear - 27 oC"
+        };
+
+        for(String detail : weatherinfo) {
+            weatherview.append(detail + "\n\n\n");
+        }
+    }
+}
